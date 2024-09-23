d391519c7f1a601aff48cff228ddac35b4239a29
MatheusEngSoft <matheus.engsof@gmail.com>
2024-09-23 17:12:43 -0300

    ajuste jsx for tsx

--- src/components/PerfilVetAdmin.tsx
+++ src/components/PerfilVetAdmin.tsx
@@ -1,17 +1,7 @@
-<<<<<<< HEAD:src/components/PerfilVetAdmin.jsx
 import React, { useState, useEffect } from 'react';
-import LargeVetAdmin from './LargeVetAdmin'; 
-import SmallVetAdmin from './SmallVetAdmin'; 
-=======
-import React from 'react'
-import { PiChatsLight } from "react-icons/pi";
-import { LuHotel, LuFacebook, LuInstagram, LuLinkedin, LuLayoutDashboard } from "react-icons/lu";
-import { IoLocationOutline } from "react-icons/io5";
-import { TbPencilMinus, TbPhotoEdit } from "react-icons/tb";
-import logo from '../assets/images/logo.svg'
-import Rating from './Rating.tsx';
+import LargeVetAdmin from './LargeVetAdmin.tsx'; 
+import SmallVetAdmin from './SmallVetAdmin.tsx'; 
 
->>>>>>> 6dbeec72268fcd13bfc6418ea483d50ecb54990d:src/components/PerfilVetAdmin.tsx
 
 function PerfilVetAdmin() {
   const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
--- src/components/SmallVetAdmin.tsx
+++ src/components/SmallVetAdmin.tsx
@@ -4,26 +4,26 @@ import { LuHotel, LuFacebook, LuInstagram, LuLinkedin, LuLayoutDashboard } from 
 import { IoLocationOutline } from "react-icons/io5";
 import { TbPencilMinus, TbPhotoEdit } from "react-icons/tb";
 import logo from '../assets/images/logo.svg'
-import Rating from './Rating';
+import Rating from './Rating.tsx';
 
 
-function LargeVetAdmin() {
+function PerfilVetAdmin() {
   return (
-    <div className="bg-white">
+    <div className="bg-white flex flex-col w-[640px] mx-auto">
       <div className='flex justify-between items-center w-full h-[70] bg-[#F5F5F5]'>
           <a href="" className='mx-auto'><img src={logo} alt=""  className='w-48 h-16'/></a>
           <LuLayoutDashboard size={40} className='mr-5'/>
       </div>
-      <div className="flex justify-center items-center h-screen">
-        <div className="relative w-[1204px] h-[544px] shadow-md rounded-xl overflow-hidden">
-          <div className="absolute top-32 left-3 rounded-full z-10">
+      <div className="flex justify-center items-center h-screen w-[450px] mx-auto">
+        <div className="relative w-[768px] h-[544px] shadow-md rounded-xl overflow-hidden">
+          <div className="absolute top-32 left-32 rounded-full z-10">
             <img src="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className=" rounded-full object-cover w-[250px] h-[250px]"/>
           </div>
           <div className="flex justify-end w-full h-1/2 bg-gray-400 p-2">
             <a href=""><TbPhotoEdit size={40} color='black'/></a>
           </div>
           <div className="bg-white h-full p-4">
-            <div className="ml-72">
+            <div className="ml-32 mt-28">
               <h2 className="text-3xl font-bold m-0 font-fraunces no-underline">Matheus Santos de Oliveira</h2>
               <p className="mt-1.5 text-slate-300">
                 <div className="flex justify-between items-center w-full">
@@ -32,28 +32,34 @@ function LargeVetAdmin() {
                     <span className="inline-block p-1 rounded-[100px] text-sm font-semibold bg-slate-200 text-slate-600">Clinica</span>
                     <span className="inline-block p-1 rounded-[100px] text-sm font-semibold bg-slate-200 text-slate-600">Clinica</span>
                   </div>
-                  <div className="mr-3">
-                    <a href='./Chat' className='no-underline'><button className="bg-[#5C2574] font-medium shadow-lg text-white text-base p-1 rounded-lg cursor-pointer flex items-center gap-1 font-roboto hover:transition-transform hover:duration-300 hover:ease-in-out"><PiChatsLight size={30}/>Fale com este profissional</button></a>
-                  </div>
                 </div>
               </p>
-              <div className="flex items-center justify-between gap-2 mt-2">
-                <div className="flex flex-col text-xs font-sans">
-                  <p className='flex gap-1'><LuHotel/>Domicílio</p>
-                  <p className='flex gap-1'><IoLocationOutline/>Belo Horizonte, MG</p>
-                </div>
-                <div className="flex items-center">
-                  <Rating/>
-                </div>
-                <div className="flex items-center font-bold text-base">Valor da consulta</div>
-                <div className="flex items-center"><a href="" className="no-underline decoration-inherit mr-4 "><TbPencilMinus size={25} color='black'/></a></div>
-              </div>
             </div>
           </div>
         </div>
       </div>
-      <div className="flex justify-center items-center">
-        <div className='relative w-[1204px] h-96 shadow-md rounded-xl overflow-auto bg-white p-4 m-0'>
+      <div className='flex justify-center items-center'>
+        <div className='relative w-[450px] h-96 shadow-md rounded-xl overflow-auto bg-white p-4 m-0'>
+          <div className="flex flex-col items-center justify-center gap-2 mt-5">
+            <div className="flex flex-col text-xs font-sans">
+              <p className='flex gap-1'><LuHotel/>Domicílio</p>
+              <p className='flex gap-1'><IoLocationOutline/>Belo Horizonte, MG</p>
+            </div>
+            <div className="flex items-center">
+              <Rating/>
+            </div>
+            <div className='flex flex-row gap-4'>
+              <div className="flex items-center font-bold text-base">Valor da consulta</div>
+              <div className="flex items-center"><a href="" className="no-underline decoration-inherit mr-4 "><TbPencilMinus size={25} color='black'/></a></div>
+            </div>
+            <div className="mt-5">
+              <a href='./Chat' className='no-underline'><button className="bg-[#5C2574] font-medium shadow-lg text-white text-base p-1 rounded-lg cursor-pointer flex items-center gap-1 font-roboto hover:transition-transform hover:duration-300 hover:ease-in-out"><PiChatsLight size={30}/>Fale com este profissional</button></a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center items-center mt-5">
+        <div className='relative w-[450px] h-96 shadow-md rounded-xl overflow-auto bg-white p-4'>
           <div className='flex justify-between items-center'>
             <h1 className='text-3xl font-sans font-bold text-black ml-3'>Informações do Profissional</h1>
             <a href="" className='no-underline'><TbPencilMinus size={30} color='black'/></a>
@@ -79,7 +85,7 @@ function LargeVetAdmin() {
         </div>
       </div>
       <div className="flex justify-center items-center mt-5">
-        <div className='relative w-[1204px] h-96 shadow-md rounded-xl overflow-auto bg-white p-4'>
+        <div className='relative w-[450px] h-96 shadow-md rounded-xl overflow-auto bg-white p-4'>
           <div className='flex justify-between items-center'>
             <h1 className='text-3xl font-sans font-bold text-black ml-3'>Formação Acadêmica</h1>
             <a href="" className='no-underline'><TbPencilMinus size={30} color='black'/></a>
@@ -87,7 +93,7 @@ function LargeVetAdmin() {
         </div>
       </div>
       <div className="flex justify-center items-center mt-5">
-        <div className='relative w-[1204px] h-96 shadow-md rounded-xl overflow-auto bg-white p-4'>
+        <div className='relative w-[450px] h-96 shadow-md rounded-xl overflow-auto bg-white p-4'>
           <div className='flex justify-between items-center'>
             <h1 className='text-3xl font-sans font-bold text-black ml-3'>Experiência Profissional</h1>
             <a href="" className='no-underline'><TbPencilMinus size={30} color='black'/></a>
@@ -95,7 +101,7 @@ function LargeVetAdmin() {
         </div>
       </div>
       <div className="flex justify-center items-center mt-5">
-        <div className='relative w-[1204px] h-96 shadow-md rounded-xl overflow-auto bg-white p-4'>
+        <div className='relative w-[450px] h-96 shadow-md rounded-xl overflow-auto bg-white p-4'>
           <div className='flex justify-between items-center'>
             <h1 className='text-3xl font-sans font-bold text-black ml-3'>Certificados</h1>
             <a href="" className='no-underline'><TbPencilMinus size={30} color='black'/></a>
@@ -103,7 +109,7 @@ function LargeVetAdmin() {
         </div>
       </div>
       <div className="flex justify-center items-center mt-5">
-        <div className='relative w-[1204px] h-96 shadow-md rounded-xl overflow-auto bg-white p-4'>
+        <div className='relative w-[450px] h-96 shadow-md rounded-xl overflow-auto bg-white p-4'>
           <div className='flex justify-between items-center'>
             <h1 className='text-3xl font-sans font-bold text-black ml-3'>Atendimento</h1>
             <a href="" className='no-underline'><TbPencilMinus size={30} color='black'/></a>
@@ -139,4 +145,4 @@ function LargeVetAdmin() {
   );
 };
 
-export default LargeVetAdmin
+export default PerfilVetAdmin
